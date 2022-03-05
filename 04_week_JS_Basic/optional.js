ec2a773a92c369c30575d26c7cb8eb8d7b75338c
David93Cluj <puscasdavid93@gmail.com>
2022-03-05 18:36:22 +0200

    add

---
+++ 04_week_JS_Basic/optional.js
@@ -0,0 +1,80 @@
+// https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+function addition(a, b) {
+	return a + b
+}
+console.log(addition(2,3))
+
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+function convert(minutes) {
+	return minutes * 60
+}
+console.log(convert(5))
+
+// https://edabit.com/challenge/NAQhEoxbofPidLxm9
+
+function addition(num) {
+	return num + 1
+}
+console.log(addition(2))
+
+
+
+// https://edabit.com/challenge/QaApgtePE6QrCZ64o
+function getFirstValue(arr) {
+	return arr[0]
+}
+console.log(getFirstValue([1, 2, 3]))
+
+
+// https://edabit.com/challenge/Q2j5FTFtsk7PdzrQk
+function remainder(x, y) {
+    var rest = x % y
+    return rest
+   }
+   console.log(remainder(2,6))
+
+
+// https://edabit.com/challenge/8Qg78sf5SNDEANKti
+function animals(chickens, cows, pigs) {
+	return chickens * 2 + cows * 4 + pigs * 4
+}
+console.log(animals(2,3,5))
+
+
+// https://edabit.com/challenge/iBQYbSHZGhpktLRdn
+
+
+function divisibleByFive(n) {
+	var rest = n % 5
+	if (rest === 0) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+console.log(divisibleByFive(12))
+
+
+// https://edabit.com/challenge/yHGowWucg3k2kJdZ4
+
+function comp(str1, str2) {
+	return str1.length == str2.length
+}
+console.log(comp("AB", "CD"))
+
+
+// https://edabit.com/challenge/kLa2w7m4h849k8rmW
+function sayHelloBye(name, num) {
+	if (num == 1) {
+		return "Hello, " + name
+	}
+	else if (num == 0) {
+		return "Bye, " + name
+	}
+}
+console.log(sayHelloBye("Alex",1))
+
+
+
